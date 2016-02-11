a6a0622faf9b11db128b4eb93ed21189d9fd5b8b
Vimukthi Bandara Wickramasinghe <vbwickramasinghe@gmail.com>
2016-02-12 01:06:34 +0800

    Ohh

--- components/Child.js
+++ components/Child.js
@@ -1,28 +1,25 @@
 /**
  * Created by vimukthi on 2/10/16.
  */
-'use strict';
+/**
+ * Created by vimukthi on 2/10/16.
+ */
+'use strict'
 
 var noflo = require("noflo");
 
-function child(input, x) {
-    if (input && Array.isArray(input)) {
-        return input[x];
-    }
-    return input;
-}
-
-exports.getComponent = () => {
+exports.getComponent = function() {
     var component = new noflo.Component;
-    component.description = "This component receives data and send nth child on output port";
+    component.description = "This component receives data on a single input\
+  port and sends the same data out to the output port";
 
     // Register ports and event handlers
-    component.inPorts.add('in', { datatype: 'all' }, (event, payload) => {
+    component.inPorts.add('in', { datatype: 'all' }, function(event, payload) {
         switch (event) {
             case 'data':
                 // Forward data when we receive it.
                 // Note: send() will connect automatically if needed
-                return component.outPorts.out.send(child(payload[0], payload[1]));
+                return component.outPorts.out.send(payload);
             case 'disconnect':
                 // Disconnect output port when input port disconnects
                 return component.outPorts.out.disconnect();
